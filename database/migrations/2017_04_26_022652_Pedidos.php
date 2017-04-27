1b7bfdb2a2cd929d6977df9a5e0f5b14ac6e89ed
Adilson Alves Mattos <adilson.day@hotmail.com>
2017-04-26 23:12:20 -0300

    migração de banco para heroku

---
+++ database/migrations/2017_04_26_022652_Pedidos.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Pedidos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos_entrega', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('produto');
+            $table->text('descricao');
+            $table->string('estado');
+            $table->string('cidade');
+            $table->string('bairro');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Pedidos');
+    }
+}
